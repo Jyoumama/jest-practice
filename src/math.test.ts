5692d0e6d402e273902ead2d92e6d13b83e31edf
jyouma <juri19771211@gmail.com>
2025-01-08 20:55:36 +0900

    src/math.test.tsに0(ゼロ）の場合のaddとmultiplyのテストパターンを追加

--- src/math.test.ts
+++ src/math.test.ts
@@ -8,3 +8,11 @@ test("1 + 2 = 3", () => {
 test("2 * 3 = 6", () => {
   expect(multiply(2, 3)).toBe(6);
 });
+
+test("0 + 0 = 0", () => {
+  expect(add(0, 0)).toBe(0);
+});
+
+test("1 * 0 = 0", () => {
+  expect(multiply(1, 0)).toBe(0);
+});
